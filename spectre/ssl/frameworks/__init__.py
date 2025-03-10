b88bd5e0156d48b82c767790a0642509b5679e05
cclaess <c.h.b.claessens@student.tue.nl>
2025-03-10 17:16:37 +0100

    Bug fixes

--- spectre/ssl/frameworks/__init__.py
+++ spectre/ssl/frameworks/__init__.py
@@ -1,3 +1,3 @@
-from spectre.ssl.frameworks.dino import DINO
-from spectre.ssl.frameworks.mae import MAE
-from spectre.ssl.frameworks.clip import CLIP
+from .dino import DINO
+from .mae import MAE
+from .clip import CLIP
