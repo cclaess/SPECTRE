75f784b4d1478c63f624073f7f3cc6bba1a08fe9
cclaess <c.h.b.claessens@student.tue.nl>
2025-03-12 11:21:37 +0100

    Combine base and specific transformations

--- spectre/ssl/transforms/__init__.py
+++ spectre/ssl/transforms/__init__.py
@@ -1,4 +1,3 @@
-from .base_transform import BaseTransform
 from .dino_transform import DINOTransform
 from .mae_transform import MAETransform
 from .clip_transform import CLIPTransform
--- spectre/ssl/transforms/dino_transform.py
+++ spectre/ssl/transforms/dino_transform.py
@@ -2,6 +2,12 @@ from typing import Tuple
 
 from monai.transforms import (
     Compose,
+    LoadImaged,
+    EnsureChannelFirstd,
+    ScaleIntensityRanged,
+    Orientationd,
+    Spacingd,
+    ResizeWithPadOrCropd,
     RandSpatialCropSamplesd,
     CopyItemsd,
     Resized,
@@ -18,6 +24,20 @@ class DINOTransform(Compose):
         ):
         super().__init__(
             [
+                LoadImaged(keys=("image",)),
+                EnsureChannelFirstd(keys=("image",), channel_dim="no_channel"),
+                ScaleIntensityRanged(
+                    keys=("image",), 
+                    a_min=-1000, 
+                    a_max=1000, 
+                    b_min=0.0, 
+                    b_max=1.0, 
+                    clip=True
+                ),
+                Orientationd(keys=("image",), axcodes="RAS"),
+                Spacingd(keys=("image",), pixdim=(0.75, 0.75, 1.5), mode=("bilinear",)),
+                ResizeWithPadOrCropd(keys=("image",), spatial_size=(384, 384, 256)),
+                
                 # Take equal amount of crops from the same subject as could 
                 # be taken without overlap to avoid data-loading overhead
                 RandSpatialCropSamplesd(
