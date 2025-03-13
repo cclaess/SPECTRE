9f53a8316044c5f0cb3833f3b78adc50e420e4b1
cclaess <c.h.b.claessens@student.tue.nl>
2025-03-13 15:24:57 +0100

    Initialization bug fix

--- spectre/models/vits.py
+++ spectre/models/vits.py
@@ -188,8 +188,8 @@ class VisionTransformer(nn.Module):
         # this fn left here for compat with downstream users
         if isinstance(m, nn.Linear):
             nn.init.trunc_normal_(m.weight, std=.02)
-        if hasattr(m, 'bias') and m.bias is not None:
-            nn.init.zeros_(m.bias)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
         elif hasattr(m, 'init_weights'):
             m.init_weights()
 
