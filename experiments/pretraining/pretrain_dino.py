36537582d4fe4a8f4607d19e26da6bf6a05eb6c5
cclaess <c.h.b.claessens@student.tue.nl>
2025-03-17 13:24:14 +0100

    Add beta arguments to AdamW optimizer

--- experiments/pretraining/pretrain_dino.py
+++ experiments/pretraining/pretrain_dino.py
@@ -120,6 +120,7 @@ def main(cfg):
     optimizer = AdamW(
         model.parameters(),
         lr=cfg.optim.lr,
+        betas=(cfg.optim.beta1, cfg.optim.beta2),
     )
 
     # calculate number of steps for training
