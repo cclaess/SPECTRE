63d3de24ea9e7f5185cef9937ae72c3121507827
cclaess <c.h.b.claessens@tue.nl>
2025-06-26 14:53:21 +0200

    Update dataset assertions to include 'inspect' for report inclusion

--- spectre/utils/dataloader.py
+++ spectre/utils/dataloader.py
@@ -33,7 +33,7 @@ def get_dataloader(
 
     if include_reports:
         assert set(datasets).issubset({"ct_rate", "merlin", "inspect"}), (
-            "When include_reports is True, only 'ct_rate' and 'merlin' datasets are allowed.")
+            "When include_reports is True, only 'ct_rate', 'merlin', and 'inspect' datasets are allowed.")
     if include_labels:
         assert set(datasets).issubset({"abdomen_atlas", "abdomenct_1k"}), (
             "When include_labels is True, only 'abdomen_atlas' and 'abdomenct_1k' datasets are allowed.")
