a3784c7a0b22d9cb1d5e2cf02b431ddded67a0a3
cclaess <c.h.b.claessens@tue.nl>
2025-08-11 10:53:12 +0200

    Add subset filtering to dataset initialization and improve error handling

--- spectre/data/total_segmentator.py
+++ spectre/data/total_segmentator.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from typing import Callable, List, Union, Dict
 
+import pandas as pd
 from monai.data import Dataset
 
 from spectre.data.cache_dataset import CacheDataset
@@ -10,43 +11,19 @@ from spectre.data.gds_dataset import GDSDataset
 
 LABEL_GROUPS = {
     "cardiac": [
-        "heart", 
-        "aorta", 
-        "pulmonary_vein",
-        "brachiocephalic_trunk",
-        "subclavian_artery_left",
-        "subclavian_artery_right",
-        "common_carotid_artery_left",
-        "common_carotid_artery_right",
-        "brachiocephalic_vein_left",
-        "brachiocephalic_vein_right",
-        "atrial_appendage_left",
-        "superior_vena_cava",
-        "inferior_vena_cava",
-        "portal_vein_and_splenic_vein",
-        "iliac_artery_left",
-        "iliac_artery_right",
-        "iliac_vena_left",
-        "iliac_vena_right",
+        "heart", "aorta", "pulmonary_vein", "brachiocephalic_trunk", "subclavian_artery_left",
+        "subclavian_artery_right", "common_carotid_artery_left", "common_carotid_artery_right",
+        "brachiocephalic_vein_left", "brachiocephalic_vein_right", "atrial_appendage_left",
+        "superior_vena_cava", "inferior_vena_cava", "portal_vein_and_splenic_vein",
+        "iliac_artery_left", "iliac_artery_right", "iliac_vena_left", "iliac_vena_right",
     ],
     "muscles": [
-        "humerus_left",
-        "humerus_right",
-        "fibula",  # not found in the dataset
+        "humerus_left", "humerus_right", "fibula",  # not found in the dataset
         "tibia",  # not found in the dataset
-        "femur_left",
-        "femur_right",
-        "gluteus_maximus_left",
-        "gluteus_maximus_right",
-        "gluteus_medius_left",
-        "gluteus_medius_right",
-        "gluteus_minimus_left",
-        "gluteus_minimus_right",
-        "autochthon_left",
-        "autochthon_right",
-        "iliopsoas_left",
-        "iliopsoas_right",
-        "quadriceps_femoris_left",  # not found in the dataset
+        "femur_left", "femur_right", "gluteus_maximus_left", "gluteus_maximus_right",
+        "gluteus_medius_left", "gluteus_medius_right", "gluteus_minimus_left",
+        "gluteus_minimus_right", "autochthon_left", "autochthon_right", "iliopsoas_left",
+        "iliopsoas_right", "quadriceps_femoris_left",  # not found in the dataset
         "quadriceps_femoris_right",  # not found in the dataset
         "thigh_medial_compartment_left",  # not found in the dataset
         "thigh_medial_compartment_right",  # not found in the dataset
@@ -57,86 +34,26 @@ LABEL_GROUPS = {
         "brain",
     ],
     "organs": [
-        "spleen",
-        "kidney_left",
-        "kidney_right",
-        "gallbladder",
-        "liver",
-        "stomach",
-        "pancreas",
-        "adrenal_gland_left",
-        "adrenal_gland_right",
-        "lung_lower_lobe_left",
-        "lung_lower_lobe_right",
-        "lung_middle_lobe_right",
-        "lung_upper_lobe_left",
-        "lung_upper_lobe_right",
-        "esophagus",
-        "trachea",
-        "thyroid_gland",
-        "small_bowel",
-        "deodenum",
-        "colon",
-        "urinary_bladder",
-        "prostate",
-        "kydney_cyst_left",
-        "kydney_cyst_right",
+        "spleen", "kidney_left", "kidney_right", "gallbladder", "liver", "stomach",
+        "pancreas", "adrenal_gland_left", "adrenal_gland_right", "lung_lower_lobe_left",
+        "lung_lower_lobe_right", "lung_middle_lobe_right", "lung_upper_lobe_left",
+        "lung_upper_lobe_right", "esophagus", "trachea", "thyroid_gland", "small_bowel",
+        "duodenum", "colon", "urinary_bladder", "prostate", "kidney_cyst_left",
+        "kidney_cyst_right",
     ],
     "ribs": [
-        "rib_left_1",
-        "rib_left_2",
-        "rib_left_3",
-        "rib_left_4",
-        "rib_left_5",
-        "rib_left_6",
-        "rib_left_7",
-        "rib_left_8",
-        "rib_left_9",
-        "rib_left_10",
-        "rib_left_11",
-        "rib_left_12",
-        "rib_right_1",
-        "rib_right_2",
-        "rib_right_3",
-        "rib_right_4",
-        "rib_right_5",
-        "rib_right_6",
-        "rib_right_7",
-        "rib_right_8",
-        "rib_right_9",
-        "rib_right_10",
-        "rib_right_11",
-        "rib_right_12",
-        "sternum",
-        "costal_cartilages",
+        "rib_left_1", "rib_left_2", "rib_left_3", "rib_left_4", "rib_left_5", "rib_left_6",
+        "rib_left_7", "rib_left_8", "rib_left_9", "rib_left_10", "rib_left_11", "rib_left_12",
+        "rib_right_1", "rib_right_2", "rib_right_3", "rib_right_4", "rib_right_5", "rib_right_6",
+        "rib_right_7", "rib_right_8", "rib_right_9", "rib_right_10", "rib_right_11", 
+        "rib_right_12", "sternum", "costal_cartilages",
     ],
     "vertebrae": [
-        "sacrum",
-        "vertebrae_S1",
-        "vertebrae_T1",
-        "vertebrae_T2",
-        "vertebrae_T3",
-        "vertebrae_T4",
-        "vertebrae_T5",
-        "vertebrae_T6",
-        "vertebrae_T7",
-        "vertebrae_T8",
-        "vertebrae_T9",
-        "vertebrae_T10",
-        "vertebrae_T11",
-        "vertebrae_T12",
-        "vertebrae_L1",
-        "vertebrae_L2",
-        "vertebrae_L3",
-        "vertebrae_L4",
-        "vertebrae_L5",
-        "vertebrae_C1",
-        "vertebrae_C2",
-        "vertebrae_C3",
-        "vertebrae_C4",
-        "vertebrae_C5",
-        "vertebrae_C6",
-        "vertebrae_C7",
+        "sacrum", "vertebrae_S1", "vertebrae_T1", "vertebrae_T2", "vertebrae_T3", "vertebrae_T4",
+        "vertebrae_T5", "vertebrae_T6", "vertebrae_T7", "vertebrae_T8", "vertebrae_T9",
+        "vertebrae_T10", "vertebrae_T11", "vertebrae_T12", "vertebrae_L1", "vertebrae_L2",
+        "vertebrae_L3", "vertebrae_L4", "vertebrae_L5", "vertebrae_C1", "vertebrae_C2",
+        "vertebrae_C3", "vertebrae_C4", "vertebrae_C5", "vertebrae_C6", "vertebrae_C7",
     ],
 }
 
@@ -145,10 +62,19 @@ def _initialize_dataset(
     include_labels: bool = False,
     label_groups: Union[str, List[str]] = [
         "Cardiac", "Muscles", "Organs", "Ribs", "Vertebrae"
-    ]
+    ],
+    subset: str = "train",
 ) -> List[Dict[str, str]]:
     
     image_paths = Path(data_dir).glob(os.path.join("*", "ct.nii.gz"))
+
+    # Filter metadata for the specified subset
+    meta = pd.read_csv(os.path.join(data_dir, "meta.csv"), sep=";")
+    if subset not in ["train", "val", "test"]:
+        raise ValueError(f"Invalid subset: {subset}. Choose from 'train', 'val', or 'test'.")
+    meta = meta[meta["split"] == subset]
+    image_ids = meta["image_id"].tolist()
+    image_paths = [path for path in image_paths if path.parent.name in image_ids]
 
     if include_labels:
         label_groups = list(label_groups)
@@ -189,9 +115,10 @@ class TotalSegmentatorDataset(Dataset):
         label_groups: Union[str, List[str]] = [
             "Cardiac", "Muscles", "Organs", "Ribs", "Vertebrae"
         ],
-        transform: Callable = None
+        transform: Callable = None,
+        subset: str = "train"
     ):
-        data = _initialize_dataset(data_dir, include_labels, label_groups)
+        data = _initialize_dataset(data_dir, include_labels, label_groups, subset)
         super().__init__(data=data, transform=transform)
 
 
@@ -204,9 +131,10 @@ class TotalSegmentatorCacheDataset(CacheDataset):
         label_groups: Union[str, List[str]] = [
             "Cardiac", "Muscles", "Organs", "Ribs", "Vertebrae"
         ],
+        subset: str = "train",
         transform: Callable = None
     ):
-        data = _initialize_dataset(data_dir, include_labels, label_groups)
+        data = _initialize_dataset(data_dir, include_labels, label_groups, subset)
         super().__init__(data=data, transform=transform, cache_dir=cache_dir)
 
 
@@ -220,7 +148,8 @@ class TotalSegmentatorGDSDataset(GDSDataset):
         label_groups: Union[str, List[str]] = [
             "Cardiac", "Muscles", "Organs", "Ribs", "Vertebrae"
         ],
+        subset: str = "train",
         transform: Callable = None,
     ):
-        data = _initialize_dataset(data_dir, include_labels, label_groups)
+        data = _initialize_dataset(data_dir, include_labels, label_groups, subset)
         super().__init__(data=data, transform=transform, cache_dir=cache_dir, device=device)
