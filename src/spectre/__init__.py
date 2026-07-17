6734fce56b0ddda0f348cb4b47fdeaf5c5f5ed41
cclaess <c.h.b.claessens@tue.nl>
2026-07-17 12:02:11 +0200

    Bump version to 0.2.1

--- src/spectre/__init__.py
+++ src/spectre/__init__.py
@@ -22,7 +22,7 @@ from . import transforms
 from . import ssl
 from . import utils
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Cris Claessens"
 __email__ = "c.h.b.claessens@tue.nl"
 
